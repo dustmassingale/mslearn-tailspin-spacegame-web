a3e64fd9d71a8caba80aea2c3c3bdab9bf7dc865
Dustin Massingale <dustin.massingale@cerecore.net>
2020-01-15 08:33:47 -0600

    Commit of tutorial pipeline

---
+++ .profile
@@ -0,0 +1,1 @@
+export sonar-scanner="C:\Users\fxi6103\Downloads\sonar-scanner-cli-4.2.0.1873-windows.zip\sonar-scanner-4.2.0.1873-windows\bin"
---
+++ SonarCloudSecurityScan.bat
@@ -0,0 +1,7 @@
+C:\Users\fxi6103\Downloads\sonar-scanner\sonar-scanner-4.2.0.1873-windows\bin\sonar-scanner.bat
+\
+  -D"sonar.projectKey=dustmassingale_mslearn-tailspin-spacegame-web" \
+  -D"sonar.organization=dustmassingale" \
+  -D"sonar.sources=." \
+  -D"sonar.host.url=https://sonarcloud.io" \
+  -D"sonar.login=97558dd2127d31d469f4a7f9ff1ab5249ea180fa"
